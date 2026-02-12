cfe974ab929b28b4eeb2d33a93f6d455db814651
Saltro <2373117747@qq.com>
2026-02-12 19:34:55 +0800

    feat: 更新主页

--- logs.js
+++
@@ -1,4 +0,0 @@
-let card = document.getElementById('card')
-let singleLogCard = document.getElementsByClassName('log-card')[0]
-let verticalLine = document.getElementById('vertical-line')
-verticalLine.style.left = ((card.clientWidth - singleLogCard.clientWidth) / 2 + 15) + 'px'
